f1b96c42138afd90f5836b31b9e426d0d84b8b65
Koji <48003543+0ji@users.noreply.github.com>
2021-03-09 23:56:33 -0500

    Initial commit

---
+++ app/src/main/java/com/cst2335/cst2335finalproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cst2335.cst2335finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // change content view here^
+        // koji was here :)
+
+    }
+}
