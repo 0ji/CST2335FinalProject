e9b7245aade73bca5011dbcb4818c0ade3d43fd4
Koji <48003543+0ji@users.noreply.github.com>
2021-03-14 23:37:24 -0400

    initial commit car db

--- app/src/main/java/com/cst2335/cst2335finalproject/CarDBActivity.java
+++ app/src/main/java/com/cst2335/cst2335finalproject/CarDBActivity.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CarDBActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.car_db_main);
         // change content view here^
         // koji was here :)
 
