6ef74986b232b2dcdacb44014072259f58176c8e
Koji <48003543+0ji@users.noreply.github.com>
2021-03-23 19:58:16 -0400

    finished basic layout for milestone 1 (Reqs 1, 2, 3, 11, 13)

--- app/src/main/java/com/cst2335/cst2335finalproject/CarDBActivity.java
+++ app/src/main/java/com/cst2335/cst2335finalproject/CarDBActivity.java
@@ -1,17 +1,171 @@
 package com.cst2335.cst2335finalproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+/**
+ * This class handles all the code in car_db_main.xml.
+ */
 public class CarDBActivity extends AppCompatActivity {
 
+    String[] carBrands = {"Honda", "Toyota", "Tesla", "Ford"};
+    private ArrayList<CarItem> carsList = new ArrayList<>();
+    private CarListAdapter carsAdapter = new CarListAdapter();
+    SQLiteDatabase db;
+
+    /**
+     * Create function for when the instance is created
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.car_db_main);
-        // change content view here^
-        // koji was here :)
 
+        ListView carListView = findViewById(R.id.listViewCars);
+        carListView.setAdapter(carsAdapter);
+
+        loadCarsFromDatabase();
+
+        // build the helpAlertBuilder
+        // TODO: change alert to be translatable
+        AlertDialog.Builder helpAlertBuilder = new AlertDialog.Builder(this);
+        helpAlertBuilder.setTitle("Car database instructions")
+        .setMessage("Enter a car model in the search field. Click search to bring up the list of " +
+                "cars. Select a car in the list to bring up car details, where you can add it to" +
+                "your database or shop for the car.")
+        .setPositiveButton("Understood", (click, arg) -> {});
+
+        // create the helpAlert alert dialog
+        AlertDialog helpAlert = helpAlertBuilder.create();
+        helpAlert.show(); // show the dialog
+
+        // create ArrayAdapter with list of car names, suggests car names when typing
+        // taken from https://stackoverflow.com/a/20989919
+        ArrayAdapter<String> carBrandAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.select_dialog_item, carBrands);
+        AutoCompleteTextView searchView = findViewById(R.id.carSearchView);
+        searchView.setThreshold(1);
+        searchView.setAdapter(carBrandAdapter);
+        searchView.setTextColor(Color.GRAY);
+
+        // set onClickListeners for buttons
+        ProgressBar searchProgBar = findViewById(R.id.progressBar);
+
+        // search Button
+        Button btnSearch = findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener( e -> {
+            String searchText = searchView.getText().toString();
+
+            // toast to show something
+            Toast.makeText(getApplicationContext(), "You searched `" + searchText + "`",
+                    Toast.LENGTH_SHORT).show();
+            searchProgBar.setProgress(100);
+        });
+
+        // database view button
+        Button btnDB = findViewById(R.id.btnDatabase);
+        // TODO: change functionality to view database fragment
+        btnDB.setOnClickListener( e -> {
+            Snackbar.make(findViewById(R.id.constraintView), "Added to database.",
+                    Snackbar.LENGTH_LONG)
+                    .setAction("Undo", e2 -> {
+                        // TODO: remove car from database
+                    })
+                    .show();
+        });
+
+        // help button
+        Button btnHelp = findViewById(R.id.btnHelp);
+        btnHelp.setOnClickListener(e -> {
+            helpAlert.show();
+        });
+        // temporarily inflate list TODO: remove this
+
+        // car list view
+        carListView.setOnItemClickListener( (parent, view, pos, id) -> {
+            AlertDialog.Builder carDialogBuilder = new AlertDialog.Builder(this);
+            CarItem selectedItem = carsList.get(pos);
+
+            carDialogBuilder.setTitle("Car details")
+
+                .setMessage("The selected row is " + pos)
+
+                .setPositiveButton("Add", (click, arg) -> {
+                   // add item to database
+                })
+
+                .setNegativeButton("Close", (click, arg) -> {})
+
+                .create().show();
+        });
+    }
+
+    /**
+     * Function to load cars from existing database, if any.
+     */
+    private void loadCarsFromDatabase() {
+        // TODO: implement this!! make load from sharedprefs database.
+
+        for (int i = 0; i < 15; i++) {
+        carsList.add(new CarItem(1, 1, "make_test", 1 + " :model_test"));
+        }
+        carsAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * CarsListAdapter is made to show a list of cars, for search.
+     */
+    private class CarListAdapter extends BaseAdapter {
+
+        public int getCount() { return carsList.size();}
+
+        public Object getItem(int position) { return carsList.get(position);
+        }
+
+        public long getItemId(int position) { return (long) position; }
+
+        public View getView(int position, View old, ViewGroup parent)
+        {
+            View newView = old;
+            // load layout inflater
+            LayoutInflater inflater = getLayoutInflater();
+            // gets message from array
+            CarItem newCarItem = (CarItem) getItem(position);
+
+            //make a new row view:
+
+            //set what the text should be for this row:
+            newView = inflater.inflate(R.layout.car_db_item, parent, false);
+            TextView inflaterMakeText = newView.findViewById(R.id.carMake);
+            TextView inflaterModelText = newView.findViewById(R.id.carModel);
+            inflaterMakeText.setText(newCarItem.getMake());
+            inflaterMakeText.setText(newCarItem.getModel());
+
+            //return it to be put in the table
+            return newView;
+        }
     }
 }
---
+++ app/src/main/java/com/cst2335/cst2335finalproject/CarItem.java
@@ -0,0 +1,31 @@
+package com.cst2335.cst2335finalproject;
+
+public class CarItem {
+    private int _id;
+    private int makeID;
+    private String make;
+    private String model;
+
+    public CarItem(int _id, int makeID, String make, String model) {
+        this._id = _id;
+        this.makeID = makeID;
+        this.make = make;
+        this.model = model;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public int getMakeID() {
+        return makeID;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
