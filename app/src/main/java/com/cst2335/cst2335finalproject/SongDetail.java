fe21f1a4ee27aed1f75713425199fa27e0bade91
Meih125 <77461047+Meih125@users.noreply.github.com>
2021-04-11 03:56:47 -0400

    Update SongDetail.java

--- app/src/main/java/com/cst2335/cst2335finalproject/SongDetail.java
+++ app/src/main/java/com/cst2335/cst2335finalproject/SongDetail.java
@@ -20,8 +20,6 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import com.cst2335.cst2335finalproject.R;
-
 
 public class SongDetail extends Fragment {
     private TextView artistNameView;
@@ -46,6 +44,9 @@ public class SongDetail extends Fragment {
     boolean isTablet;
 
 
+
+    @SuppressWarnings( "deprecation" )
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         prevData=getArguments();
@@ -57,28 +58,28 @@ public class SongDetail extends Fragment {
         isTablet=prevData.getBoolean("isTablet");
 
 
-        View view=inflater.inflate(com.cst2335.cst2335finalproject.R.layout.activity_song_details_page,container,false);
+        View view=inflater.inflate(R.layout.activity_song_details_page,container,false);
 
 
         artistNameView=view.findViewById(R.id.artistName);
         artistIdView=view.findViewById(R.id.artistId);
         songIdView=view.findViewById(R.id.songId);
         songTitleView=view.findViewById(R.id.SongTitle);
-         SaveOrDeleteButton =view.findViewById(R.id.SaveDB);
-         searchSong=view.findViewById(R.id.searchButton);
-         searchArtist=view.findViewById(R.id.ArtistIdbtn);
-         artistNameView.setText("ArtistName: "+artistName);
-         artistIdView.setText("ArtistId: "+artistId);
-         songIdView.setText("SongId: "+songId);
-         songTitleView.setText("Song title: "+songTitle);
+        SaveOrDeleteButton =view.findViewById(R.id.SaveDB);
+        searchSong=view.findViewById(R.id.searchButton);
+        searchArtist=view.findViewById(R.id.ArtistIdbtn);
+        artistNameView.setText("ArtistName: "+artistName);
+        artistIdView.setText("ArtistId: "+artistId);
+        songIdView.setText("SongId: "+songId);
+        songTitleView.setText("Song title: "+songTitle);
 
         searchSong.setOnClickListener(d->{
-                String url=SearchSongUrl+songId;
-                Intent searchGoogle=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                startActivity(searchGoogle);
+                    String url=SearchSongUrl+songId;
+                    Intent searchGoogle=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(searchGoogle);
 
                 }
-                );
+        );
 
         searchArtist.setOnClickListener(d->{
                     String urlArtist=SearchArtistUrl+songId;
@@ -90,54 +91,53 @@ public class SongDetail extends Fragment {
 
         if(isDeleteButton ==false){
             SaveOrDeleteButton.setText(R.string.Songster_Save);
-
             SaveOrDeleteButton.setOnClickListener(v->{
 
                 AlertDialog.Builder alert=new AlertDialog.Builder(getActivity());
                 alert.setTitle(R.string.SongDetail_FavouriteListConfirmation_Add).
-                        setPositiveButton(R.string.confirm,(Click, args)->{
+                        setPositiveButton(R.string.confirm,(Click,args)->{
+                            //create instance of SongsterList
                             SongsterList songster= new SongsterList();
                             ContentValues cv= new ContentValues();
-                            com.cst2335.cst2335finalproject.SongOpener songDB=new com.cst2335.cst2335finalproject.SongOpener(getActivity());
+                            SongOpener songDB=new SongOpener(getActivity());
                             db=songDB.getWritableDatabase();
 
                             boolean isSaved=false;
-                            String [] columns={ com.cst2335.cst2335finalproject.SongOpener.COL_ID, com.cst2335.cst2335finalproject.SongOpener.COL_ARTISTNAME, com.cst2335.cst2335finalproject.SongOpener.COL_ARTISTID, com.cst2335.cst2335finalproject.SongOpener.COL_SONGID, com.cst2335.cst2335finalproject.SongOpener.COL_SONGTITLE};
-                            Cursor results = db.query(false, com.cst2335.cst2335finalproject.SongOpener.TABLE_NAME, columns, null, null, null, null, null, null);
-                            int songIdColIndex = results.getColumnIndex(com.cst2335.cst2335finalproject.SongOpener.COL_SONGID);
+                            String [] columns={ SongOpener.COL_ID, SongOpener.COL_ARTISTNAME, SongOpener.COL_ARTISTID, SongOpener.COL_SONGID, SongOpener.COL_SONGTITLE};
+                            Cursor results = db.query(false, SongOpener.TABLE_NAME, columns, null, null, null, null, null, null);
+                            int songIdColIndex = results.getColumnIndex(SongOpener.COL_SONGID);
                             while(results.moveToNext()){
                                 String songIdOld=results.getString(songIdColIndex);
                                 if(songIdOld.equals(songId)){
-                                    Toast.makeText(getActivity(), R.string.duplicateMessage,Toast.LENGTH_LONG).show();
+                                    Toast.makeText(getActivity(),R.string.duplicateMessage,Toast.LENGTH_LONG).show();
                                     isSaved=true;
                                     break;
                                 }
 
-
                             }
                             if(!isSaved) {
-                                cv.put(com.cst2335.cst2335finalproject.SongOpener.COL_ARTISTNAME, artistName);
-                                cv.put(com.cst2335.cst2335finalproject.SongOpener.COL_ARTISTID, artistId);
-                                cv.put(com.cst2335.cst2335finalproject.SongOpener.COL_SONGID, songId);
-                                cv.put(com.cst2335.cst2335finalproject.SongOpener.COL_SONGTITLE, songTitle);
-                                db.insert(com.cst2335.cst2335finalproject.SongOpener.TABLE_NAME, null, cv);
-                                Toast toast=Toast.makeText(getActivity(), R.string.ConfirmationMessageofSave,Toast.LENGTH_LONG);
-                                View view1 = toast.getView();
-                                view1.setBackgroundColor(Color.parseColor("#FF000000"));
-                                TextView text = (TextView) view1.findViewById(android.R.id.message);
-                                text.setTextColor(Color.parseColor("#FFFFFFFF"));
-                                text.setTextSize(20);
+                                cv.put(SongOpener.COL_ARTISTNAME, artistName);
+                                cv.put(SongOpener.COL_ARTISTID, artistId);
+                                cv.put(SongOpener.COL_SONGID, songId);
+                                cv.put(SongOpener.COL_SONGTITLE, songTitle);
+                                db.insert(SongOpener.TABLE_NAME, null, cv);
+                                Toast toast=Toast.makeText(getActivity(),R.string.ConfirmationMessageofSave,Toast.LENGTH_LONG);
+//                                View view1 = toast.getView();
+//                                view1.setBackgroundColor(Color.parseColor("#FF000000"));
+//                                TextView text = (TextView) view1.findViewById(android.R.id.message);
+//                                text.setTextColor(Color.parseColor("#FFFFFFFF"));
+//                                text.setTextSize(20);
                                 toast.show();
                             }
 
                         }).
-                        setNegativeButton(R.string.decline,(click, args)->{
-                            Toast toast=Toast.makeText(getActivity(), R.string.nothingChanged,Toast.LENGTH_LONG);
-                            View view1 = toast.getView();
-                            view1.setBackgroundColor(Color.parseColor("#FF000000"));
-                            TextView text = (TextView) view1.findViewById(android.R.id.message);
-                            text.setTextColor(Color.parseColor("#FFFFFFFF"));
-                            text.setTextSize(20);
+                        setNegativeButton(R.string.decline,(click,args)->{
+                            Toast toast=Toast.makeText(getActivity(),R.string.nothingChanged,Toast.LENGTH_LONG);
+//                            View view1 = toast.getView();
+//                            view1.setBackgroundColor(Color.parseColor("#FF000000"));
+//                            TextView text = (TextView) view1.findViewById(android.R.id.message);
+//                            text.setTextColor(Color.parseColor("#FFFFFFFF"));
+//                            text.setTextSize(20);
                             toast.show();
 
 
@@ -153,45 +153,39 @@ public class SongDetail extends Fragment {
 
                 AlertDialog.Builder alert= new AlertDialog.Builder(getActivity());
                 alert.setTitle(R.string.AskingMessageToDelete).
-                        setPositiveButton(R.string.confirm,(Click, args)->{
-                            com.cst2335.cst2335finalproject.SongOpener songDB=new com.cst2335.cst2335finalproject.SongOpener(getActivity());
+                        setPositiveButton(R.string.confirm,(Click,args)->{
+                            SongOpener songDB=new SongOpener(getActivity());
                             db=songDB.getWritableDatabase();
 
-                           songDB.deleteFromDB(songId);
+                            songDB.deleteFromDB(songId);
 
-                           if(!isTablet){
-
-
-                           Intent returnIntent = new Intent();
-                           returnIntent.putExtra("result", 1);
-                           getActivity().setResult(Activity.RESULT_OK, returnIntent);
-                           getActivity().finish();
+                            if(!isTablet){
 
 
-                           }
+                                Intent returnIntent = new Intent();
+                                returnIntent.putExtra("result", 1);
+                                getActivity().setResult(Activity.RESULT_OK, returnIntent);
+                                getActivity().finish();
+
+
+                            }
                             String str = getString(R.string.ConfirmationMessage_Of_Deletion);
                             Toast.makeText(getActivity(), songTitle + " " + str, Toast.LENGTH_LONG).show();
                             parentApp.getSupportFragmentManager().beginTransaction().remove(this).commit();
                         }).
-                        setNegativeButton(R.string.decline,(click, args)->{
-                            Toast toast=Toast.makeText(getActivity(), R.string.nothingChanged,Toast.LENGTH_LONG);
-                            View view1 = toast.getView();
-                            view1.setBackgroundColor(Color.parseColor("#FF000000"));
-                            TextView text = (TextView) view1.findViewById(android.R.id.message);
-                            text.setTextColor(Color.parseColor("#FFFFFFFF"));
-                            text.setTextSize(20);
+                        setNegativeButton(R.string.decline,(click,args)->{
+                            Toast toast=Toast.makeText(getActivity(),R.string.nothingChanged,Toast.LENGTH_LONG);
+//                            View view1 = toast.getView();
+//                            view1.setBackgroundColor(Color.parseColor("#FF000000"));
+//                            TextView text = (TextView) view1.findViewById(android.R.id.message);
+//                            text.setTextColor(Color.parseColor("#FFFFFFFF"));
+//                            text.setTextSize(20);
                             toast.show();
                         }).create().show();
             });
-
-
         }
-
-
-         return view;
-
+        return view;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -202,4 +196,8 @@ public class SongDetail extends Fragment {
         super.onAttach(context);
         parentApp = (AppCompatActivity)context;
     }
+
+
+
+
 }
