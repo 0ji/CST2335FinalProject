7f293bb31458313fe45f32541e32eab133f4dd61
Meih125 <77461047+Meih125@users.noreply.github.com>
2021-04-11 03:57:44 -0400

    Update FavouriteList.java

--- app/src/main/java/com/cst2335/cst2335finalproject/FavouriteList.java
+++ app/src/main/java/com/cst2335/cst2335finalproject/FavouriteList.java
@@ -1,5 +1,9 @@
 package com.cst2335.cst2335finalproject;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -15,11 +19,6 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-import com.cst2335.cst2335finalproject.R;
 
 import java.util.ArrayList;
 
@@ -45,7 +44,7 @@ public class FavouriteList extends AppCompatActivity {
         loadDataFromDB();
         myAdapter=new MySongListAdapter();
         listview.setAdapter(myAdapter);
-       pullRefresher=findViewById(R.id.refresher);
+        pullRefresher=findViewById(R.id.refresher);
 
         backButton.setOnClickListener((e)->
         {
@@ -53,7 +52,7 @@ public class FavouriteList extends AppCompatActivity {
 
         });
 
-       isTablet=findViewById(R.id.frameLayout)!=null;
+        isTablet=findViewById(R.id.frameLayout)!=null;
         String str=getString(R.string.youSelected);
         if(isTablet){
             pullRefresher.setOnRefreshListener(() -> {
@@ -71,8 +70,8 @@ public class FavouriteList extends AppCompatActivity {
 
             AlertDialog.Builder alert= new AlertDialog.Builder(this);
             alert.setTitle(R.string.Songster_moreInfoAboutSong).
-                            setMessage(str+" "+selectedArtist.getSongTitle()).
-                            setPositiveButton(R.string.confirm,(click, arg)->
+                    setMessage(str+" "+selectedArtist.getSongTitle()).
+                    setPositiveButton(R.string.confirm,(click,arg)->
                     {
                         dataTopass.putString("id",String.valueOf(selectedArtist.getId()));
                         dataTopass.putString("ArtistName",selectedArtist.getArtistName());
@@ -95,15 +94,15 @@ public class FavouriteList extends AppCompatActivity {
                             nextActivity.putExtras(dataTopass);
                             startActivityForResult(nextActivity,1); //start activity
                         }
-                    }).setNegativeButton(R.string.decline,(click, arg)->{
+                    }).setNegativeButton(R.string.decline,(click,arg)->{
 
-                        Toast toast= Toast.makeText(getApplicationContext(), R.string.nothingChanged, Toast.LENGTH_LONG);
-                        View view = toast.getView();
-                        view.setBackgroundColor(Color.parseColor("#FF000000"));
-                        TextView text = (TextView) view.findViewById(android.R.id.message);
-                        text.setTextColor(Color.parseColor("#FFFFFFFF"));
-                        text.setTextSize(20);
-                        toast.show();
+                Toast toast= Toast.makeText(getApplicationContext(), R.string.nothingChanged, Toast.LENGTH_LONG);
+//                View view = toast.getView();
+//                view.setBackgroundColor(Color.parseColor("#FF000000"));
+//                TextView text = (TextView) view.findViewById(android.R.id.message);
+//                text.setTextColor(Color.parseColor("#FFFFFFFF"));
+//                text.setTextSize(20);
+                toast.show();
 
             }).create().show();
             return true;
@@ -113,79 +112,79 @@ public class FavouriteList extends AppCompatActivity {
     }
 
     @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data){
-      super.onActivityResult(requestCode, resultCode, data);
-       if(requestCode == 1) {
-           if(resultCode== Activity.RESULT_OK) {
-                 favList.remove(positionSaved);
-                 myAdapter.notifyDataSetChanged();
-               }
-           }
-       }
-        private void loadDataFromDB(){
-
-            SongOpener songDB=new SongOpener(this);
-            db=songDB.getWritableDatabase();
-            String [] columns={ SongOpener.COL_ID, SongOpener.COL_ARTISTNAME, SongOpener.COL_ARTISTID, SongOpener.COL_SONGID, SongOpener.COL_SONGTITLE};
-            Cursor results = db.query(false, SongOpener.TABLE_NAME, columns, null, null, null, null, null, null);
-
-            int idColIndex = results.getColumnIndex( SongOpener.COL_ID);
-            int artistNameColIndex = results.getColumnIndex( SongOpener.COL_ARTISTNAME);
-            int artistIdColIndex = results.getColumnIndex(SongOpener.COL_ARTISTID);
-            int songIdColIndex = results.getColumnIndex(SongOpener.COL_SONGID);
-            int songTitleIndex = results.getColumnIndex(SongOpener.COL_SONGTITLE);
-
-            while(results.moveToNext()){
-                long id=results.getLong(idColIndex);
-                String artistName=results.getString(artistNameColIndex);
-                String artistId=results.getString(artistIdColIndex);
-                String songId=results.getString(songIdColIndex);
-                String songTitle=results.getString(songTitleIndex);
-
-                favList.add(new Artist(id,artistName,artistId,songId,songTitle));
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1) {
+            if(resultCode== Activity.RESULT_OK) {
+                favList.remove(positionSaved);
+                myAdapter.notifyDataSetChanged();
             }
         }
+    }
+    private void loadDataFromDB(){
 
-        private class MySongListAdapter extends BaseAdapter {
+        SongOpener songDB=new SongOpener(this);
+        db=songDB.getWritableDatabase();
+        String [] columns={ SongOpener.COL_ID, SongOpener.COL_ARTISTNAME, SongOpener.COL_ARTISTID, SongOpener.COL_SONGID, SongOpener.COL_SONGTITLE};
+        Cursor results = db.query(false, SongOpener.TABLE_NAME, columns, null, null, null, null, null, null);
 
-            @Override
-            public int getCount() {
+        int idColIndex = results.getColumnIndex( SongOpener.COL_ID);
+        int artistNameColIndex = results.getColumnIndex( SongOpener.COL_ARTISTNAME);
+        int artistIdColIndex = results.getColumnIndex(SongOpener.COL_ARTISTID);
+        int songIdColIndex = results.getColumnIndex(SongOpener.COL_SONGID);
+        int songTitleIndex = results.getColumnIndex(SongOpener.COL_SONGTITLE);
+
+        while(results.moveToNext()){
+            long id=results.getLong(idColIndex);
+            String artistName=results.getString(artistNameColIndex);
+            String artistId=results.getString(artistIdColIndex);
+            String songId=results.getString(songIdColIndex);
+            String songTitle=results.getString(songTitleIndex);
+
+            favList.add(new Artist(id,artistName,artistId,songId,songTitle));
+        }
+    }
+
+    private class MySongListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
             return favList.size();
         }
 
-            @Override
-            public Artist getItem(int position) {
+        @Override
+        public Artist getItem(int position) {
             return favList.get(position);
         }
 
-            @Override
-            public long getItemId(int position) {
+        @Override
+        public long getItemId(int position) {
             return getItem(position).getId();
         }
 
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                LayoutInflater inflater=getLayoutInflater();
-                Artist artist= (Artist) getItem(position);
-
-                View artistView=inflater.inflate(R.layout.artist_row,parent,false);
-                TextView songId=artistView.findViewById(R.id.SongIdRow);
-                TextView title=artistView.findViewById(R.id.artistTitleRow);
-                TextView artistId=artistView.findViewById(R.id.artistIdRow);
-                TextView artistName=artistView.findViewById(R.id.artistName);
-                songId.setText(artist.getSongId());
-                title.setText(artist.getSongTitle());
-                artistId.setText(artist.getArtistId());
-                artistName.setText(artist.getArtistName());
-
-                return artistView;
-            }
-        }
-
         @Override
-        protected void onResume() {
-            super.onResume();
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater=getLayoutInflater();
+            Artist artist= (Artist) getItem(position);
 
+            View artistView=inflater.inflate(R.layout.artist_row,parent,false);
+            TextView songId=artistView.findViewById(R.id.SongIdRow);
+            TextView title=artistView.findViewById(R.id.artistTitleRow);
+            TextView artistId=artistView.findViewById(R.id.artistIdRow);
+            TextView artistName=artistView.findViewById(R.id.artistName);
+            songId.setText(artist.getSongId());
+            title.setText(artist.getSongTitle());
+            artistId.setText(artist.getArtistId());
+            artistName.setText(artist.getArtistName());
 
+            return artistView;
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+    }
 }
