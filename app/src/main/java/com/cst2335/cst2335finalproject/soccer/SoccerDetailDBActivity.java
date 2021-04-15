ace4859a4c335e481902d72c98bd49e7f3eee27c
PiggyCat <chun0073@algonquinlive.com>
2021-04-15 18:07:45 -0400

    soccer nav updated

--- app/src/main/java/com/cst2335/cst2335finalproject/soccer/SoccerDetailDBActivity.java
+++ app/src/main/java/com/cst2335/cst2335finalproject/soccer/SoccerDetailDBActivity.java
@@ -17,6 +17,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.cst2335.cst2335finalproject.songster.SongsterSearch;
 import com.cst2335.cst2335finalproject.trivia.MainActivity;
@@ -69,24 +70,32 @@ public class SoccerDetailDBActivity extends AppCompatActivity {
             int id = item.getItemId();
             switch (id) {
                 case R.id.soccer_goToMain:
+                    Toast.makeText(this,"Main",Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(this, MainActivity.class);
                     startActivity(intent);
                     break;
                 case R.id.soccer_goToTrivia:
+                    Toast.makeText(this,"Trivia",Toast.LENGTH_SHORT).show();
                     Intent intent2 = new Intent(this, TriviaActivity.class);
                     startActivity(intent2);
                     break;
                 case R.id.soccer_goToCarDB:
+                    Toast.makeText(this,"CarDB",Toast.LENGTH_SHORT).show();
                     Intent intent3 = new Intent(this, CarDBActivity.class);
                     startActivity(intent3);
                     break;
                 case R.id.soccer_goToSoccerMain:
+                    Toast.makeText(this,"Soccer",Toast.LENGTH_SHORT).show();
                     Intent intent4 = new Intent(this, SoccerMainActiv.class);
                     startActivity(intent4);
+                    break;
                 case R.id.soccer_goToSong:
+                    Toast.makeText(this,"SongStar",Toast.LENGTH_SHORT).show();
                     Intent intent5 = new Intent(this, SongsterSearch.class);
                     startActivity(intent5);
+                    break;
                 case R.id.soccer_previous:
+                    Toast.makeText(this,"Previous",Toast.LENGTH_SHORT).show();
                     Intent intent6 = new Intent(this, MainActivity.class);
                     setResult(500, intent6);
                     finish();
